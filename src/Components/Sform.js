227be8aab65c925c99cd4f94299ffc7661d27ad6
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-08-17 12:04:15 +0530

    fbase

--- src/Components/Sform.js
+++ src/Components/Sform.js
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
-import {auth,provider} from './config';
-import { signInWithPopup } from "firebase/auth"; 
-import Home from '../Pages/Home';
 
 function App() {
-const[value,setValue]=useState('');
+const[setValue]=useState('');
   const handleClick=()=>
   {
-    signInWithPopup(auth,provider).then((data)=>{
-setValue(data.user.email)
-localStorage.setItem ("email",data.user.email)
-    })
+    
   }
   useEffect(()=>{
     setValue(localStorage.getItem('email'));
